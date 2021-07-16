17e7a93ad04c4cb43ddea185e7bac801e65cff86
Cauã <cauakathdev@gmail.com>
2021-07-16 11:24:14 -0300

    Vetor contendo todos os nomes e cores dos stats

---
+++ src/pages/Homepage/stats.ts
@@ -0,0 +1,26 @@
+export const StatsColor = [
+  {
+    name: "hp",
+    color: "#FF5959"
+  },
+  {
+    name: "attack",
+    color: "#F08030"
+  },
+  {
+    name: "defense",
+    color: "#FAE078"
+  },
+  {
+    name: "special-attack",
+    color: "#9DB7F5"
+  },
+  {
+    name: "special-defense",
+    color: "#A7DB8D"
+  },
+  {
+    name: "speed",
+    color: "#FA92B2"
+  }
+]
